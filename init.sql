6f733eba0c3311066796829307df19af3f4f7e11
Hasnaee12 <132940621+Hasnaee12@users.noreply.github.com>
2024-05-29 23:01:34 +0100

    h11

--- init.sql
+++ init.sql
@@ -17,7 +17,3 @@ CREATE TABLE IF NOT EXISTS reservation (
     nombre_nuits INT NOT NULL,
     FOREIGN KEY (email) REFERENCES user(email)
 );
-
--- Insérez quelques données de test
-INSERT INTO user (name, email, password) VALUES ('salma', 'salma@email.com', 'password123'), ('Bob', 'bob@example.com', 'password456');
-INSERT INTO reservation (email, nom_maison, date, nombre_personnes, nombre_nuits) VALUES ('alice@example.com', 'Maison de Alice', '2024-06-01', 4, 3), ('bob@example.com', 'Maison de Bob', '2024-06-10', 2, 2);
